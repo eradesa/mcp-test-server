51ba592463b44cd72a552ea06b6c04323d325f17
eradesa <eranga.de.saram@gmail>
2026-04-17 00:18:13 +0530

    add other servers13

---
+++ src/mcpserver/__init__.py
@@ -0,0 +1,4 @@
+# mcpserver/__init__.py
+from .deployment import register as register_deployment
+from .prompt_server import register as register_prompts
+from .resources import register as register_resources
